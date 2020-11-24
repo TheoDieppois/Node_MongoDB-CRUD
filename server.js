6032547ad53019d003e0a6504ec4a487b2521ac9
Théo Dieppois <contact@theodieppois.fr>
2020-11-24 16:08:17 +0100

    routes

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const port = process.env.PORT || 8000;
 const mongoose = require('mongoose'); 
 
 
-const URI = "mongodb+srv://groupeE:passwordgroupee@cluster0.6wggo.mongodb.net/test?retryWrites=true&w=majority"; 
+const Lieux = require("./model/lieu")
+const URI = "mongodb+srv://groupeE:passwordgroupee@cluster0.6wggo.mongodb.net/airbnb?retryWrites=true&w=majority"; 
  
+
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,9 +18,33 @@ mongoose.connect(URI, {
 .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world !!');
+
+app.get('/', async (req, res) => {
+    await Lieux.find(((err, lieu) => {
+        if (err){
+            res.send(err); 
+        } else {
+            res.json(lieu);   
+        }
+    }))
 });
+
+app.get('/:id', async (req, res) => {
+    res.send(req.params.id)
+});
+
+app.get('/:date', async (req, res) => {
+    res.send(req.params.id)
+});
+
+app.delete('/:id', (req, res) => {
+    
+})
+
+app.put('/:id', (req, res) => {
+    
+})
+
 
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
